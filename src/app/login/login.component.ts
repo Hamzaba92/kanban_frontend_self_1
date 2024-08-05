f7c67f616c986788b984a05a9488a8b8b10b9000
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-08-05 20:44:33 +0200

    loggin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  username = '';
+  password = '';
+
+login(){
+
+}
 
 }

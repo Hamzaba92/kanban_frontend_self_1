e0d87221997a5da05cd2244335aed281ff4bbd39
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-08-13 14:02:05 +0200

    add-task function

--- src/app/kanban/landingpage/landingpage.component.ts
+++ src/app/kanban/landingpage/landingpage.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import {
   CdkDragDrop,
   CdkDrag,
@@ -9,22 +9,35 @@ import {
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+
+
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [MatCardModule, CdkDropListGroup, CdkDropList, CdkDrag],
+  imports: [MatCardModule, CdkDropListGroup, CdkDropList, CdkDrag, MatInputModule,
+     MatFormFieldModule, FormsModule, MatButtonModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
-
-  
 
   todo: string[] = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done: string[] = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
   progress: string[] = ['Learning', 'Walking'];
+
+  addTask(taskInput: HTMLInputElement): void {
+    const task = taskInput.value;
+    if (task) {
+      this.todo.push(task);
+      taskInput.value = '';
+    }
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {

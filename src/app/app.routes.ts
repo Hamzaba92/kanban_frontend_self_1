1f36038708955dd7a4b968ac3538f8a498ef5973
Hamzaba92 <153004965+Hamzaba92@users.noreply.github.com>
2024-08-13 12:53:22 +0200

    implement drag & drop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { LandingpageComponent } from './kanban/landingpage/landingpage.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'board', component: LandingpageComponent},
 ];
--- src/app/kanban/landingpage/landingpage.component.ts
+++ src/app/kanban/landingpage/landingpage.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
+
+  
+
+  todo: string[] = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done: string[] = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  progress: string[] = ['Learning', 'Walking'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 
 }
